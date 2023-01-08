2c45ef16a750a1b06f4cf9c3f7014da7fa639248
mrroberttables <jduffey222@gmail.com>
2023-01-08 16:41:24 -0500

    Add Cramer Cookbook
    Add the Cramer cookbook to the site.

---
+++ CramerCookbook/lists.js
@@ -0,0 +1,19 @@
+let authors = new Map();
+authors.set('L', 'Leona');
+authors.set('V', 'Virginia');
+authors.set('A', 'Arlene');
+authors.set('K', 'Karen');
+authors.set('J', 'Janice');
+authors.set('M', 'Mary');
+
+
+let categories = new Map();
+categories.set(0, 'Appetizers');
+categories.set(1, 'Breads and Rolls');
+categories.set(2, 'Cakes and Cookies');
+categories.set(3, 'Desserts');
+categories.set(4, 'Jams & Jellies, Pickles & Relishes');
+categories.set(5, 'Main Dishes');
+categories.set(6, 'Soups and Salads');
+categories.set(7, 'Vegetables');
+categories.set(8, 'Miscellaneous');
