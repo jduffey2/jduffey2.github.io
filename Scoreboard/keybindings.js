5676225af6c7499fb63f8c8e2fab9b535ac553ce
jduffey <jduffey222@gmail.com>
2023-04-12 22:05:04 -0400

    Add the rest of Moms recipes

---
+++ Scoreboard/keybindings.js
@@ -0,0 +1,11 @@
+let keybindings = {
+	HOMESCORE: "q",
+	VISITORSCORE: "w",
+	HOMETEAMSCORE: "1",
+	VISITORTEAMSCORE: "2",
+	PERIOD: "p",
+	CLOCK: "s",
+	RED: "ArrowRight",
+	GREEN: "ArrowLeft",
+	RIDINGSTOP: "a,"
+}
